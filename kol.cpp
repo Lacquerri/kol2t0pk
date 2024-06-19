b6c8c03b8fcffb6c763496aa2037786fb6b819e3
Bartłomiej Pawlik <148866688+Lacquerri@users.noreply.github.com>
2024-06-19 12:45:06 +0200

    Add files via upload

---
+++ kol.cpp
@@ -0,0 +1,153 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
+
+#pragma once
+#include <cstring>
+#include <cstdio>
+#include <ctype.h>
+#include <cmath>
+#include <iostream>
+#include <cassert>
+
+enum class Dostepnosc
+{
+	Dostepna,
+	Niedostepna,
+};
+
+class Autor {
+private:
+	const char* nazwisko;
+public:
+	void setKlient(const char* nazwa) {
+		assert(nazwa != nullptr);
+		nazwisko = nazwa;
+	}
+	const char* getNazwa() const { return nazwisko; }
+	Autor(const char* nazwa) : nazwisko(nazwa) {}
+};
+
+
+class Ksiazka
+{
+private:
+	const char* tytul;
+	int strony;
+	Dostepnosc dostep;
+	Autor* autor;
+
+	static Ksiazka* wzorzec;
+public:
+	virtual double cena() { return ((double)get_strony()); }
+	//																		KONSTRUKTORY
+	Ksiazka() {
+		assert(wzorzec != nullptr); //////////
+		set_all(wzorzec->get_tytul(), wzorzec->get_strony(), wzorzec->get_dostep(), wzorzec->get_Autor());
+	};
+
+	Ksiazka(const char* nowyTytul, int ilosc, Dostepnosc nowyDostep, Autor* nowyAutor) {
+		assert(nowyAutor != nullptr); //////////
+		set_all(nowyTytul, ilosc, nowyDostep, nowyAutor);
+	};
+
+	//																		SETERY
+	void set_tytul(const char* nowyTytul) {
+		assert(nowyTytul != nullptr);
+		tytul = nowyTytul;
+	}
+
+	void set_strony(int ilosc) {
+		if (ilosc < 5) { strony = 5; }
+		else strony = ilosc;
+	}
+
+	void set_dostep(Dostepnosc nowyDostep) { dostep = nowyDostep; }
+
+	void set_Autor(Autor* nowyAutor) {
+		assert(nowyAutor != nullptr);
+		autor = nowyAutor;
+	}
+
+	static void set_wzorzec(Ksiazka* wzor) { wzorzec = wzor; }
+
+	void set_all(const char* nowyTytul, int ilosc, Dostepnosc nowyDostep, Autor* nowyAutor) {
+		set_tytul(nowyTytul);
+		set_strony(ilosc);
+		set_dostep(nowyDostep);
+		set_Autor(nowyAutor);
+	}
+
+	//																		GETERY
+	const char* get_tytul() const { return tytul; }
+	int get_strony() { return strony; }
+	Dostepnosc get_dostep() { return dostep; }
+	Autor* get_Autor() { return autor; }
+	
+	explicit operator const char* () {
+
+		const char* title = get_tytul();
+
+		return (title);
+
+	}
+
+	bool operator==(Ksiazka& ksi) // str autor dostep
+	{
+
+		if (this != &ksi) {
+			if (this->get_strony() != ksi.get_strony())
+				return false;
+
+			if (this->get_Autor() != ksi.get_Autor())
+				return false;
+			if (this->get_dostep() != ksi.get_dostep())
+				return false;
+		}
+		return true;
+
+	}
+};
+Ksiazka* Ksiazka::wzorzec = nullptr; 
+
+class EBook : public Ksiazka {
+private:
+	int rozmiar;
+public:
+	void set_rozmiar(int size) {
+		if (size < 1) rozmiar = 1;
+		else rozmiar = size;
+	}
+
+	int get_rozmiar() { return rozmiar; }
+
+	EBook(const char* nowyTytul, int ilosc, Dostepnosc nowyDostep, Autor* nowyAutor, int size) : Ksiazka(nowyTytul, ilosc, nowyDostep, nowyAutor) {
+		set_rozmiar(size);
+	}
+
+	double cena() override { return ((double)get_strony() - ((double)get_strony()*0.25)); }
+	
+
+};
+
+
+int main() {
+	Autor Bartlomiej = "Pawlik";
+	Ksiazka ksiazka1("nowyTytul", 5, Dostepnosc::Dostepna, &Bartlomiej);
+	EBook EBook1("bebook", 6, Dostepnosc::Dostepna, &Bartlomiej, 10);
+	cout << ksiazka1.get_tytul() << ", " << ksiazka1.get_strony() << ", " << (bool)ksiazka1.get_dostep() << ", " << ksiazka1.get_Autor()->getNazwa()<<endl;
+	cout << ksiazka1.cena()<<endl;
+
+	Ksiazka::set_wzorzec(&ksiazka1);
+	Ksiazka ksiazka2;
+	cout << ksiazka2.get_tytul() << ", " << ksiazka2.get_strony() << ", " << (bool)ksiazka2.get_dostep() << ", " << ksiazka2.get_Autor()->getNazwa() << endl;
+	cout << ksiazka2.cena() << endl;
+
+	cout << "Czy rozne: " << (ksiazka1 == ksiazka1) << endl;
+	cout << "Czy rozne: " << (ksiazka1 == EBook1) << endl;
+
+	cout << EBook1.get_tytul() << ", " << EBook1.get_strony() << ", " << (bool)EBook1.get_dostep() << ", " << EBook1.get_Autor()->getNazwa() << endl;
+	cout << EBook1.cena() << endl << endl;
+	cout << (const char*)ksiazka1;
+
+};
